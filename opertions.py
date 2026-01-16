30a71f63fff9cfaf32bf0907c9ef94217b5afba3
naginenihari <naginenihari@gmail.com>
2026-01-16 17:10:37 +0530

    python files are created

--- opertions.py
+++ opertions.py
@@ -5,9 +5,15 @@ total = a + b
 differnce = b - a
 Multiple = a * b
 division = b / a
-floor_division = b // a
+floor_division = b // a  ## Integer division
+ ## if you want remainder we can use %
+remainders = a % b
+remainder = b % a
+
 print(total)
 print(differnce)
 print(Multiple)
 print(division)
 print(floor_division)
+print(remainders)
+print(remainder)
