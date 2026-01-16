7284e700e90ebf7bddb3e3894a8147f14eda4fab
naginenihari <naginenihari@gmail.com>
2026-01-16 17:02:48 +0530

    python files are created

---
+++ opertions.py
@@ -0,0 +1,13 @@
+a = 10
+b = 20
+
+total = a + b
+differnce = b - a
+Multiple = a * b
+division = b / a
+floor_division = b // a
+print(total)
+print(differnce)
+print(Multiple)
+print(division)
+print(floor_division)
