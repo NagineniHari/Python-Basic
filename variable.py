a98e0c2ae115f631b129ae6073e502216dc6831a
naginenihari <naginenihari@gmail.com>
2026-01-16 16:51:21 +0530

    python files are created

---
+++ variable.py
@@ -0,0 +1,50 @@
+frist_variable = 10
+num_servers = 20 ## This is an inline comment
+
+a = 10
+b = 20 
+
+## Multi line string 
+
+""" 
+This is a 
+sample 
+comments
+
+ """
+
+##Normal String
+
+"Hello World"
+'Hello World'
+'Today\'s Weather is nice'
+"Today's Weather is nice"
+
+
+## Data Types
+"""
+1. Numbers(Interger, float)
+2. String 
+3. Boolean
+4. list
+5. Tuple
+6. Dict(Dictonaries, Hashmaps)
+7.Set
+
+"""
+num_servers = 20 ## This is Interger value
+print(num_servers)
+print(type(num_servers))
+
+COST_PI= 3.14   ##This is float value
+print(type(COST_PI))
+
+num_servers = 15 ## This is Interger value
+print(num_servers)
+print(type(num_servers))
+
+server = "Ansible"  ## This is string
+print(type(server))
+
+is_prod = False
+print(type(is_prod))
