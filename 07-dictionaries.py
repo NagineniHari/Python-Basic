073baabd0e1d20282def1e2dfaa2ff95f83c1c09
naginenihari <naginenihari@gmail.com>
2026-01-20 13:49:30 +0530

    python files are updated

---
+++ 07-dictionaries.py
@@ -0,0 +1,15 @@
+d = {}
+
+d_1 = dict()
+
+
+sample = {'a':1, 'b':2}
+
+print(sample['a'],sample.get('b'))
+
+## Dictionaries are mutable datatypes in python
+sample['a'] = 20
+
+print(sample['a'],sample.get('b'))
+
+## Keys in Dictionaries once define can't be cahnged those are immutable 
---
+++ 08-string.py
@@ -0,0 +1,15 @@
+sample = "Hello, How are you doing ,how is devops course"
+sample_1 ="abc" ## ("a","b","c")
+# print(sample[8])
+# print(sample_1[0])
+## print(dir(sample))
+
+"""
+'capitalize', 'casefold', 'center', 'count', 'encode', 'endswith', 'expandtabs', 'find', 'format', 
+'format_map', 'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', 'isdigit', 'isidentifier', 'islower', 
+'isnumeric', 'isprintable', 'isspace', 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip', 
+'maketrans', 'partition', 'removeprefix', 'removesuffix', 'replace', 'rfind', 'rindex', 'rjust', 
+'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title',
+ 'translate', 'upper', 'zfill'
+
+"""
